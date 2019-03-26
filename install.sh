4f0dfbaff8d12fe49c7909494852c216544d2b46
JungHwan Park <pjhool0612@gmail.com>
2019-03-26 09:47:16 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,14 @@ cd /opt/lampp/bin
 ./mysqladmin -u root PASSWORD 1234 
 
 
+xampp에서 패스워드 설정후 phpMyAdmin 패스워드를 동일하게 변경해주어야 합니다.
+
+경로 : xampp/phpMyAdmin/config.inc.php
+
+config.inc.php 파일상에서 “$cfg[‘Servers’][$i][‘password’]” 항목을 검색후 변경한 root password 입력하고 저장 합니다.
+
+
+
 /opt/lampp/bin/mysql -u root -p1234
 
 #mysql -u user -p -e 'Your SQL Query Here' database-name
